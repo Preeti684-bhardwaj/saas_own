befceda2c4040fb937d40367f50c4f896011b9cb
Preeti684-bhardwaj <bhardwajpreeti684@gmail.com>
2024-08-02 11:38:36 +0530

    cors

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 // Add this line to handle OPTIONS requests
-app.options("*", cors(corsOptions));
+// app.options("*", cors(corsOptions));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Credentials', 'true');
