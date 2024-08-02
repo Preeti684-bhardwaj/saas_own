a46489320a60c8345fe230f9f4e0ab5f34b327fe
Preeti684-bhardwaj <bhardwajpreeti684@gmail.com>
2024-08-02 13:03:54 +0530

    cors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const errorMiddleware = require("./middlewares/error.js");
 require("dotenv").config({ path: "./.env" });
 const app = express();
 const cors = require("cors");
-const allowedOrigins =['https://aiengage.xircular.io','https://new-video-editor.vercel.app','https://stream.xircular.io']
+const allowedOrigins =['https://aiengage.xircular.io','https://new-video-editor.vercel.app']
 
 app.use(cors({
     origin: (origin, callback) => {
